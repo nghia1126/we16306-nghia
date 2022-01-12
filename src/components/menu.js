4aa8046e327a31776c67065d3145bd0a055670cd
Pham Nghia <nghiaphamvan2002@gmail.com>
2022-01-12 21:46:43 +0700

    messege

--- src/components/menu.js
+++ src/components/menu.js
@@ -9,9 +9,9 @@ const Menu = {
         <ul class="flex">
           <li><a href="" class="block py-2 px-3 text-white  hover:underline underline-offset-4 ">Trang chủ</a></li>
           <li><a href="" class="block py-2 px-3 text-white hover:underline underline-offset-4">Tuyển sinh</a></li>
-          <li><a href="" class="block py-2 px-3 text-white hover:underline underline-offset-4">Chương trình đào tạo</a></li>
-          <li><a href="" class="block py-2 px-3 text-white hover:underline underline-offset-4">Góc sinh viên</a></li>
-          <li><a href="" class="block py-2 px-3 text-white hover:underline underline-offset-4">Tuyển dụng</a></li>
+          <li><a href="./admin/dashboard" class="block py-2 px-3 text-white hover:underline underline-offset-4">Đăng kí</a></li>
+          <li><a href="" class="block py-2 px-3 text-white hover:underline underline-offset-4">Đăng nhập</a></li>
+          <li><a href="" class="block py-2 px-3 text-white hover:underline underline-offset-4">Admin</a></li>
           <form action="" class="block py-3 px-4 text-white ">
             <input type="text">
             <button class="border-5 bg-cyan-900  px-[5px] text-white hover:bg-cyan-600">TÌM KIẾM</button>

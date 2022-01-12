55b1f5db0deb8e8acd86aa05e775dc9b52291787
Pham Nghia <nghiaphamvan2002@gmail.com>
2022-01-12 21:08:51 +0700

    message

--- src/components/menu.js
+++ src/components/menu.js
@@ -7,7 +7,7 @@ const Menu = {
       </div>
       <div class="bg-[#b27b34]">
         <ul class="flex">
-          <li><a href="http://localhost:3000//assignment/src/admin/signIn.js" class="block py-2 px-3 text-white  hover:underline underline-offset-4 ">Trang chủ</a></li>
+          <li><a href="" class="block py-2 px-3 text-white  hover:underline underline-offset-4 ">Trang chủ</a></li>
           <li><a href="" class="block py-2 px-3 text-white hover:underline underline-offset-4">Tuyển sinh</a></li>
           <li><a href="" class="block py-2 px-3 text-white hover:underline underline-offset-4">Chương trình đào tạo</a></li>
           <li><a href="" class="block py-2 px-3 text-white hover:underline underline-offset-4">Góc sinh viên</a></li>
